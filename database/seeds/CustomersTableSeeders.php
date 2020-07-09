46667eafaffc8fd60cb9352ad51b9f9c2dc72b03
JosueGarrido <josue.garrido@epn.edu.ec>
2020-07-09 16:26:25 -0500

    #4 Creacion de seeders

---
+++ database/seeds/CustomersTableSeeders.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Customers;
+use Illuminate\Database\Seeder;
+
+class CustomersTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Vaciar la tabla.
+        Customers::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        //Crear customers ficticios en la tabla
+        for($i = 0; $i < 50; $i++) {
+            Customers::create([
+                'name'=> $faker->name,
+                'lastname'=> $faker->lastName,
+                'document'=> $faker->sentence,
+            ]);
+        }
+    }
+}
